37361f58e40c6a06392bdcbcc62de7533097292f
divyatamarani <divyatamarani@gmail.com>
2026-04-28 04:57:29 +0530

    Coded UC3: Display Hello with Command-Line Argument or Default Message

--- HelloApp.java
+++ HelloApp.java
@@ -1,15 +1,12 @@
 /**
- * UC2 – Display Hello with Command-Line Argument
+ * UC3 – Display Hello with Command-Line Argument or Default
  */
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, World!");
-        }
+        String name = (args.length > 0) ? args[0] : "World";
+
+        System.out.println("Hello, " + name + "!");
     }
 }
